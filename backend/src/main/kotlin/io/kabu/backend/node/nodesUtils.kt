3207bddb554c4838a727c233144642483fec73f3
bipokot <10675204+bipokot@users.noreply.github.com>
2023-07-14 21:43:47 +0600

    added guard counter into topological sort algorithm

--- backend/src/main/kotlin/io/kabu/backend/node/nodesUtils.kt
+++ backend/src/main/kotlin/io/kabu/backend/node/nodesUtils.kt
@@ -2,9 +2,11 @@ package io.kabu.backend.node
 
 
 fun sortTopologically(graph: Set<Node>): List<Node> {
+    var counter = 0
     val remainingNodes = graph.toMutableList()
     val resolvedNodes = mutableListOf<Node>()
     while (remainingNodes.isNotEmpty()) {
+        if (counter++ > 100_000) error("Topological sort went into infinite loop, remaining nodes: $remainingNodes")
         val iterator = remainingNodes.iterator()
         while (iterator.hasNext()) {
             val node = iterator.next()
